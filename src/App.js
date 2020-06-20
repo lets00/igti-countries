1753c02e06fe1131c185931b302d5c8d0c0361c1
Luís Eduardo <eduardovansilva@gmail.com>
2020-06-20 14:57:31 -0300

    Add Header component and filter coutries by name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Countries from './components/countries/Countries';
+import Header from './components/header/Header';
 
 export default class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      allCountries: []
+      allCountries: [],
+      filteredCountries: [],
+      filter: ''
     }
   }
 
@@ -14,19 +17,31 @@ export default class App extends Component {
     const data = await fetch('https://restcountries.eu/rest/v2/all')
     const json = await data.json()
     const allCountries = json.map(({ name, numericCode, flag, population }) => {
-      return { id: numericCode, name, flag, population }
+      return { id: numericCode, name, filterName: name.toLowerCase(), flag, population }
     });
     this.setState({
-      allCountries
+      allCountries,
+      filteredCountries: allCountries
+    })
+  }
+
+  handleChangeFilter = (newText) => {
+    const newTextLowerCase = newText.toLowerCase()
+    const { allCountries } = this.state
+    const filteredCountries = allCountries.filter(country => country.filterName.includes(newTextLowerCase) > 0)
+    this.setState({
+      filter: newText,
+      filteredCountries
     })
   }
 
   render() {
-    const { allCountries } = this.state
+    const { filteredCountries, filter } = this.state
     return (
       <div className="container">
         <h1>React Countries</h1>
-        <Countries countries={allCountries}/>
+        <Header filter={filter} onChangeFilter={this.handleChangeFilter}/>
+        <Countries countries={filteredCountries}/>
       </div>
     );
   }
